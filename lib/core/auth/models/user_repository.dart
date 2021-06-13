63246531fc6776bed2c000889206a4ce9c503e5c
Elena <littleyoru2000@gmail.com>
2021-06-13 22:02:24 +0200

    added more functionality to user repository

--- lib/core/auth/models/user_repository.dart
+++ lib/core/auth/models/user_repository.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'User.dart';
 import '../login/loginActions.dart';
+
+const String _AUTH_KEY = 'AuthKey';
 
 enum Status { Uninitialized, Authenticated, Authenticating, Unauthenticated }
 
@@ -8,10 +11,46 @@ class UserRepository with ChangeNotifier {
   // User? _user;
   Status _status = Status.Uninitialized;
   String _token = '';
+  // SharedPreferences? preference;
+  // UserRepository();
 
   Status get status => _status;
   // User? get authUser => _user;
   String get token => _token;
+
+  Future init() async {
+    var pref = await SharedPreferences.getInstance();
+    String token = pref.getString(_AUTH_KEY) ?? '';
+    _token = token;
+  }
+
+  // bool isUserLoggedIn() {
+  //   return _token.isNotEmpty ? true : false;
+  // }
+
+  Future<bool> setToken(String token) async {
+    var pref = await SharedPreferences.getInstance();
+    pref.setString(_AUTH_KEY, token).then((value) {
+      debugPrint('token saved');
+      return true;
+    }).catchError((err) {
+      debugPrint('error in saving token');
+      return false;
+    });
+    return false;
+  }
+
+  Future<bool> deleteToken() async {
+    var pref = await SharedPreferences.getInstance();
+    pref.setString(_AUTH_KEY, '').then((value) {
+      debugPrint('token saved');
+      return true;
+    }).catchError((err) {
+      debugPrint('error in saving token');
+      return false;
+    });
+    return false;
+  }
 
   Future<bool> signIn(data) async {
     try {
@@ -20,15 +59,25 @@ class UserRepository with ChangeNotifier {
       var res = await loginUser(data);
       debugPrint('res token $res');
       _status = Status.Authenticated;
+      _token = res;
       notifyListeners();
       debugPrint('user status $_status');
+      var isTokenSaved = await setToken(res);
       // _user = res.user;
       // _token = res.token;
-      return true;
+      return isTokenSaved;
     } catch (err) {
       _status = Status.Unauthenticated;
       notifyListeners();
       return false;
     }
+  }
+
+  Future<bool> logout() async {
+    var res = await deleteToken();
+    _status = Status.Unauthenticated;
+    _token = '';
+    notifyListeners();
+    return res;
   }
 }
