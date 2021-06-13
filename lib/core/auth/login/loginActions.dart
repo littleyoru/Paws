d8b98c3893c58e05d7df06b829f4c65a7ace4fc4
Elena <littleyoru2000@gmail.com>
2021-06-13 22:01:32 +0200

    get token from login response

--- lib/core/auth/login/loginActions.dart
+++ lib/core/auth/login/loginActions.dart
@@ -5,7 +5,7 @@ import 'dart:io';
 import '../../../constants/api_path.dart';
 import 'loginResponse.dart';
 
-Future<LoginResponse> loginUser(data) async {
+Future<String> loginUser(data) async {
   final uri = Uri.parse('$API_URL/user/login');
   var body = json.encode(data);
   final response = await http.post(uri,
@@ -22,7 +22,13 @@ Future<LoginResponse> loginUser(data) async {
     final responseJson = jsonDecode(response.body);
     final loginResponse = LoginResponse.fromJson(responseJson);
     debugPrint('login response parsed: $loginResponse');
-    return loginResponse;
+    debugPrint('login data: ${loginResponse.data?.token}');
+    var data = loginResponse.data;
+    if (data == null) {
+      throw Exception('Failure in the api - no data');
+    } else {
+      return data.token ?? '';
+    }
   } else {
     throw Exception('Failure in the api');
   }
